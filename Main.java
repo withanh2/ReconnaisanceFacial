04a6c444dfd1e372157dfa2b7bbc21ef87a79353
Withanh2 <nathan77.havard@gmail.com>
2026-05-29 09:28:19 +0200

    ajout de nom au personne

--- Main.java
+++ Main.java
@@ -10,31 +10,6 @@ public class Main {
 
     
     
-	    SimpleMatrix matExemple = new SimpleMatrix(new double[][]{
-        {1.0, 2.0, 3.0},
-        {4.0, 5.0, 6.0},
-        {7.0, 8.0, 9.0}
-        });
-			
-			
-		double[] vp = calculer_ValeurPropre(matExemple);
-
-        System.out.println("=>",vp[0],"=>",vp[1]);
-
-	}
-
-    public static double[] calculer_ValeurPropre(SimpleMatrix matrice){
-
-        var decomposition = matrice.eig();
-
-        int taille = matrice.numRows();
-        double[] valeurPropre = new double[taille];
-
-        for (int i=0; i<taille; i++){
-            valeurPropre[i] = decomposition.getEigenvalue(i).getReal();
-        }
-
-    return valeurPropre;
     }
 	
 }
