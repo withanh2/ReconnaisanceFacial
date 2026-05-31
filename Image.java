7fd9882fe8253e2cf5b4695f9a35f4f51559fca0
Withanh2 <nathan77.havard@gmail.com>
2026-05-31 12:10:28 +0200

    ajout commentaire javadoc classe : ListePropre, Image et Visages

--- Image.java
+++ Image.java
@@ -1,6 +1,10 @@
 import org.ejml.simple.SimpleMatrix;
 import java.io.*;
 
+/*
+ * @author Nathan HAVARD
+ * @brief Représente une image issue de la base de données, identifiée par sa personne, son numéro d'image, son chemin et la matrice de pixels associée.
+ */
 public class Image{
 
     //------------------------------------------------------------------------------------------------------------
@@ -17,6 +21,11 @@ public class Image{
     //------- CONSTRUCTEURS -------------------------------------------------------------------------------------- 
     //------------------------------------------------------------------------------------------------------------
 
+    /*
+     * @author Nathan HAVARD
+     * @param String chemin le chemin vers le fichier PGM de l'image
+     * @brief Constructeur d'une Image à partir d'un chemin. Extrait automatiquement l'id de la personne (dossier parent), l'id de l'image (nom du fichier) et lit la matrice de pixels.
+     */
     public Image(String chemin){
         this.chemin = chemin;
         File fichier = new File(chemin);
@@ -31,6 +40,12 @@ public class Image{
     //------------------------------------------------------------------------------------------------------------
 
 
+    /*
+     * @author Nathan HAVARD
+     * @param String chemin le chemin vers le fichier PGM à lire
+     * @return SimpleMatrix vecteur colonne (hauteur*largeur, 1) contenant les niveaux de gris de chaque pixel
+     * @brief Lit un fichier PGM binaire (format P5) et le convertit en vecteur colonne, ligne par ligne.
+     */
     private SimpleMatrix lirePGM(String chemin){
         try(DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(chemin)))){
             String format = dis.readLine();
@@ -59,34 +74,74 @@ public class Image{
     //------------------------------------------------------------------------------------------------------------
 
 
+    /*
+     * @author Nathan HAVARD
+     * @return String idPersonne identifiant de la personne (nom du dossier parent)
+     * @brief Accesseur de l'identifiant de la personne associée à l'image.
+     */
     public String getIdPersonne(){
         return idPersonne;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @param String idPersonne nouvel identifiant de la personne
+     * @brief Modifie l'identifiant de la personne associée à l'image.
+     */
     public void setIdPersonne(String idPersonne){
         this.idPersonne = idPersonne;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @return String idImage identifiant de l'image (nom du fichier sans extension)
+     * @brief Accesseur de l'identifiant de l'image.
+     */
     public String getIdImage(){
         return idImage;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @param String idImage nouvel identifiant de l'image
+     * @brief Modifie l'identifiant de l'image.
+     */
     public void setIdImage(String idImage){
         this.idImage = idImage;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @return SimpleMatrix matrix vecteur colonne contenant les niveaux de gris des pixels de l'image
+     * @brief Accesseur de la matrice de pixels.
+     */
     public SimpleMatrix getMatrix(){
         return matrix;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @param SimpleMatrix matrix nouvelle matrice de pixels
+     * @brief Modifie la matrice de pixels.
+     */
     public void setMatrix(SimpleMatrix matrix){
         this.matrix = matrix;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @return String chemin chemin du fichier image
+     * @brief Accesseur du chemin du fichier image.
+     */
     public String getChemin(){
         return chemin;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @param String chemin nouveau chemin du fichier image
+     * @brief Modifie le chemin du fichier image.
+     */
     public void setChemin(String chemin){
         this.chemin = chemin;
     }
--- Visages.java
+++ Visages.java
@@ -2,6 +2,10 @@ import org.ejml.simple.SimpleMatrix;
 import java.util.List;
 import java.util.ArrayList;
 
+/*
+ * @author Nathan HAVARD
+ * @brief Représente un ensemble de visages (images de référence) et fournit les opérations de l'ACP : construction de la matrice des vecteurs, calcul du visage moyen, centrage et matrice de covariance.
+ */
 public class Visages{
     //------------------------------------------------------------------------------------------------------------
     //------- DEFINITION DES VARIABLES ---------------------------------------------------------------------------
@@ -15,6 +19,11 @@ public class Visages{
     //------- CONSTRUCTEURS -------------------------------------------------------------------------------------- 
     //------------------------------------------------------------------------------------------------------------
 
+    /*
+     * @author Nathan HAVARD
+     * @param List<Image> images liste des images de référence à utiliser
+     * @brief Constructeur de Visages. Initialise la matrice des images en vecteurs et la matrice de covariance réduite.
+     */
     public Visages(List<Image> images){
         this.images = images;
         this.matrixVecteur = creerMatrixVecteur();
@@ -25,6 +34,11 @@ public class Visages{
     //------- FONCTION -------------------------------------------------------------------------------------------
     //------------------------------------------------------------------------------------------------------------
 
+    /*
+     * @author Nathan HAVARD
+     * @return SimpleMatrix matrice (nbPixels x nbImages) où chaque colonne est le vecteur d'une image
+     * @brief Construit la matrice contenant toutes les images sous forme de vecteurs, une image par colonne.
+     */
     private SimpleMatrix creerMatrixVecteur(){
         int nbPixels = images.get(0).getMatrix().getNumRows();
         int nbImages = images.size();
@@ -40,6 +54,11 @@ public class Visages{
         return matrice;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @return SimpleMatrix vecteur colonne (nbPixels x 1) contenant la moyenne de chaque pixel sur l'ensemble des images
+     * @brief Calcule le visage moyen : pour chaque pixel, fait la moyenne de sa valeur sur toutes les images.
+     */
     public SimpleMatrix calculerVisageMoyen(){
         int nbPixels = matrixVecteur.getNumRows();
         int nbImages = matrixVecteur.getNumCols();
@@ -56,6 +75,11 @@ public class Visages{
         return moyen;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @return SimpleMatrix matrice (nbPixels x nbImages) où chaque colonne est une image centrée
+     * @brief Centre la matrice des images en soustrayant le visage moyen à chaque colonne.
+     */
     public SimpleMatrix centrerMatrice(){
         SimpleMatrix moyen = calculerVisageMoyen();
         int nbPixels = matrixVecteur.getNumRows();
@@ -71,6 +95,11 @@ public class Visages{
         return centree;
     }
 
+    /*
+     * @author Nathan HAVARD
+     * @return SimpleMatrix matrice de covariance réduite (nbImages x nbImages) calculée comme A^T * A où A est la matrice centrée
+     * @brief Calcule la matrice de covariance réduite utilisée pour l'ACP. Stocke le résultat dans matrixD.
+     */
     public SimpleMatrix calculerMatriceCovarianceReduite(){
         SimpleMatrix A = centrerMatrice();
         this.matrixD = A.transpose().mult(A);
