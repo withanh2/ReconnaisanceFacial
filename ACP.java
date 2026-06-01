bda487e3f745af99f23b22e04dd4b8cb3779b7e2
Turchi Jules <129932583+Julestur@users.noreply.github.com>
2026-06-01 17:14:09 +0200

    Modif classe ACP ajout fonctions

--- ACP.java
+++ ACP.java
@@ -1,5 +1,8 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
+import org.ejml.simple.SimpleEVD;
 import org.ejml.simple.SimpleMatrix;  // Module qui permet de creer de matrice 
 
 public class ACP {
@@ -88,8 +91,14 @@ public class ACP {
 
 
 
-    
 
+    /**
+	 * @author Jules Turchi
+	 * @param taille entier qui correspond à la taille de la matrice
+     * @param decomposition  stocke le résultat de la fonction .eig() liste qui associe les valeurs propres aux vecteurs propres
+     * @return double[] qui contient la liste des valeurs propres
+	 * @brief Fonction qui récupère la liste des valeurs propres
+	 */
     public static double[] recuperer_ValeurPropre(int taille ,SimpleEVD<SimpleMatrix> decomposition){
 
 
@@ -99,18 +108,20 @@ public class ACP {
         for (int i=0; i<taille; i++){
             valeurPropre[i] = decomposition.getEigenvalue(i).getReal();
         }
-
-    return valeurPropre;
+        return valeurPropre;
     }
 
 
 
 
 
-
-
-
-
+    /**
+	 * @author Jules Turchi
+	 * @param taille entier qui correspond à la taille de la matrice
+     * @param decomposition  stocke le résultat de la fonction .eig() liste qui associe les valeurs propres aux vecteurs propres
+     * @return double[] qui contient la liste des vecteurs propres
+	 * @brief Fonction qui récupère la liste des vecteurs propres
+	 */
     public static double[][] recuperer_VecteurPropre( int taille, SimpleEVD<SimpleMatrix> decomposition){
 
         double[][] vecteurPropre = new double[taille][taille]; // Chaque ligne = un vecteur propre
@@ -122,20 +133,17 @@ public class ACP {
                 vecteurPropre[i][j] = vecteur.get(j, 0);
             }
         }
-
-    return vecteurPropre;
+        return vecteurPropre;
     }
 
 
-
-
-
-
-
-
-
-
-
+    /**
+	 * @author Jules Turchi
+     * @param matrice matrice d'entier 
+     * @return une ListePropre qui contient la liste des valeurs propres et vecteurs propres
+	 * @brief Fonction qui récupère la liste des vecteurs propres et vecteurs propres, classe les valeurs propres 
+     * et vecteurs propres associées par ordre croissant puis les ajoute dans un nouvel élément de la classe ListePropre	 
+    */
     public static ListePropre calculer_ValeurPropre(SimpleMatrix matrice){
 
         SimpleEVD<SimpleMatrix> decomposition = matrice.eig(); //Récupère des listes contenants les valeurs propres et les vecteur propre associé
@@ -144,7 +152,7 @@ public class ACP {
 
         int taille = matrice.numRows();
 
-        // On met les valeurs propre dans l'odre décroissant
+        // On récupère les valeurs propres et les vecteurs propres
 
         double [] liste_ValPropre = recuperer_ValeurPropre(taille,decomposition);
         double [][] liste_VectPropre = recuperer_VecteurPropre(taille,decomposition);
@@ -155,6 +163,8 @@ public class ACP {
             indices[i] = i;
         }
 
+
+        // On trie la liste des valeurs propre par ordre décroissant en gardant l'association (indice) avec vecteurs propres
         Arrays.sort(indices, (a, b) -> Double.compare(liste_ValPropre[b], liste_ValPropre[a]));
 
         double[] valeurPropre_Trie       = new double[taille];
@@ -174,36 +184,37 @@ public class ACP {
 
 
 
-
-
-
-
-
-
-
+    /**
+	 * @author Jules Turchi
+	 * @param valeurPropre_Trie tableau contenant les valeurs propres dans l'odre décroissant 
+     * @return un liste de double[] qui contient la liste des valeurs propres à garder
+	 * @brief Fonction qui récupère la liste des valeurs propres à garder pour avoir un taux de représentation supérieur au seuil
+	 */
     public static ArrayList<Double> nb_vp_a_garder(double[] valeurPropre_Trie){
 
-        double seuil = 0.95;
+        double seuil = 0.95; // Seuil pour la représentation des données
+        
         int taille = valeurPropre_Trie.length;
         double som_totale_vp = 0;
-        double som_partielle_vp = 0;
+        double som_partielle_vp = 0; 
         int continuer=1;
         int nb_vp = 0;
 
         ArrayList<Double> liste_ValPropre = new ArrayList<>();
 
-
+        // Calcul somme totale des valeurs propres (denominateur)
         for (int i = 0; i < taille; i++){
             som_totale_vp = som_totale_vp + valeurPropre_Trie[i];
         } 
 
-
+        // On ajoute des valeurs propres a prendre en compte tant que le seuil n'est pas franchis
         while ((nb_vp < taille) && (continuer==1)) {
             
             som_partielle_vp = som_partielle_vp + valeurPropre_Trie[nb_vp];
             liste_ValPropre.add(valeurPropre_Trie[nb_vp]);
             nb_vp ++;
 
+            // Condition arret
             if ((som_partielle_vp / som_totale_vp) > seuil){
                 
                 continuer = 0;
@@ -211,7 +222,6 @@ public class ACP {
             }
 
         }
-
 
         return liste_ValPropre;
 
@@ -221,14 +231,71 @@ public class ACP {
 
 
 
+
+
+
+    /**
+	 * @author Jules Turchi
+	 * @param matrice tableau contenant les valeurs propres dans l'odre décroissant
+     * @param nb_vp nb de valeur propre gardéees ie nb de d'eigenfaces à calculer 
+	 * @param listepropre valeurs et vecteurs propre de A^T*A triées par ordre décroissant 
+     * @return un liste de double[] qui contient les eigenfaces
+	 * @brief Fonction qui calcule et normalise les vecteurs propres
+	 */
+    public static ArrayList<Double> calculer_eigenface(SimpleMatrix A, int nb_vp , ListePropre listepropre ){
+
+        int taille = A.numRows(); // Taille de la matrice A 
+
+        double[][] tab_eigenface = new double[taille][nb_vp];
     
 
+        /**
+        * Notation : 
+        * X_h : vecteur propre de A^T*A
+        * V_h : eigenface, vecteur propre de A*A^T
+        */
+
+        for (int i=0; i<nb_vp ; i++){
+
+            double lambda = listepropre.valeurPropre_Trie[i];  // On récupère la valeur propre
+
+            double X_h = listepropre.vecteurPropre_Trie[i]; // On recupère un vecteur propre
+            
+
+            // On met ce vecteur sous la forme d'un double sous la forme d'une matrice colonne
+            SimpleMatrix X_h_mat = new SimpleMatrix(X_h.length,1); 
+
+            for (int j=0; j<X_h.length ; j++){
+
+                X_h_mat.set(j,0,X_h[j]);
+
+            }
+
+            SimpleMatrix V_h = A.mult(X_h_mat);  // Calcul de A*X_h
+
+
+
+            // NORMALISATION 
+
+            double norme = Math.sqrt(lambda);
+
+
+            for (int j=0; j< taille ; j++){
+
+                tab_eigenface[j][i] = X_h_mat.get(i,0) / norme;
+            }
+        }
+
+
+        return tab_eigenface;
+
+
+    }
 
 
 
 
-    
 
-        
+
     
 }
